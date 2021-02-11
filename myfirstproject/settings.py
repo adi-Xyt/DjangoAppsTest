a799fa3432b6133377426bed4630e477ffbcd0a6
adi-Xyt <57046585+adi-Xyt@users.noreply.github.com>
2021-02-11 17:49:38 -0500

    asdf

--- myfirstproject/settings.py
+++ myfirstproject/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os # new
 from pathlib import Path
 
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
